241f91d8d10456b8540e00b2666706a876016904
md kawsar alam <mdkawsaralam537@gmail.com>
2024-02-29 00:31:39 +0600

    add my potfolio website

---
+++ src/script.js
@@ -0,0 +1,33 @@
+// mouse
+document.addEventListener('DOMContentLoaded', function() {
+    const magnifyingGlass = document.querySelector('.magnifying-glass');
+
+    document.addEventListener('mousemove', function(e) {
+        const x = e.clientX;
+        const y = e.clientY;
+
+        magnifyingGlass.style.transform = `translate(${x}px, ${y}px)`;
+    });
+// header
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const crossBtn = document.getElementById('cross-btn');
+    const navbar = document.getElementById('navbar');
+
+    hamburgerBtn.addEventListener('click', function() {
+        if (window.innerWidth < 768) {
+            navbar.classList.add('translate-x-0');
+            hamburgerBtn.style.display = 'none';
+            crossBtn.style.display = 'inline-block';
+        }
+    });
+
+    crossBtn.addEventListener('click', function() {
+        if (window.innerWidth < 768) {
+            navbar.classList.remove('translate-x-0');
+            hamburgerBtn.style.display = 'inline-block';
+            crossBtn.style.display = 'none';
+        }
+    });
+});
+// service section 
+``
